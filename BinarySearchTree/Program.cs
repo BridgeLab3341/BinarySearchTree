ab0af00b72b8285399c5c66683b324a625fc3f0a
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-19 18:18:25 +0530

    [somu]Add:Added With USing Size Method.

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -9,6 +9,16 @@
             // BinaryTree<int> binary2 = new BinaryTree<int>(70);
             binary.Insert(30);
             binary.Insert(70);
+            binary.Insert(20);
+            binary.Insert(40);
+            binary.Insert(60);
+            binary.Insert(95);
+            binary.Insert(11);
+            binary.Insert(3);
+            binary.Insert(16);
+            binary.Insert(65);
+            binary.Insert(63);
+            binary.Insert(67);
             binary.Display();
             binary.GetSize();
         }
